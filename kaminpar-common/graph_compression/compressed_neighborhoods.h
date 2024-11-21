eb97be16b868767846866f514cdd74058e68107e
Daniel Salwasser <danielsalwater@gmail.com>
2024-11-21 10:57:10 +0100

    feat(graph-compression): advoid unnecesarry register shuffling

--- kaminpar-common/graph_compression/compressed_neighborhoods.h
+++ kaminpar-common/graph_compression/compressed_neighborhoods.h
@@ -436,20 +436,14 @@ private:
       return;
     }
 
-    NodeID degree;
-    bool has_intervals;
-    if constexpr (kIntervalEncoding) {
-      const auto header = marked_varint_decode<NodeID>(&node_data);
-      degree = header.first;
-      has_intervals = header.second;
-    } else {
-      degree = varint_decode<NodeID>(&node_data);
-    }
+#ifdef KAMINPAR_COMPRESSION_INTERVAL_ENCODING
+    const auto [degree, has_intervals] = marked_varint_decode<NodeID>(&node_data);
+#else
+    const NodeID degree = varint_decode<NodeID>(&node_data);
+#endif
 
     if constexpr (kHighDegreeEncoding) {
-      const bool split_neighbourhood = degree >= kHighDegreeThreshold;
-
-      if (split_neighbourhood) [[unlikely]] {
+      if (degree >= kHighDegreeThreshold) [[unlikely]] {
         decode_parts<kHasEdgeWeights, kInvokeDirectly, kParallel>(
             node_data, node, degree, std::forward<Callback>(callback)
         );
